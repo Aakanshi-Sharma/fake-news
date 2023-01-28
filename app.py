f2ac9edda7174b48bf41f1620fd4e7a4652c3fe8
Aakanshi <56083_aakanshisharma@gbpuat-tech.ac.in>
2023-01-28 19:38:37 +0530

    completed ui

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import string
+import time
 import pickle as pkl
-import numpy as np
+import numpy
 import pandas
 
-model=pkl.load(open("model.pkl", "rb"))
-ps=pkl.load(open("portStem.pkl", "rb"))
-stopingWords=pkl.load(open("stoping.pkl", "rb"))
-vectorizer=pkl.load(open("vectorizer.pkl", "rb"))
+model = pkl.load(open("model.pkl", "rb"))
+ps = pkl.load(open("portStem.pkl", "rb"))
+stopingWords = pkl.load(open("stoping.pkl", "rb"))
+vectorizer = pkl.load(open("vectorizer.pkl", "rb"))
 
 
 def stemming(text):
@@ -21,12 +22,31 @@ def stemming(text):
     return " ".join(p)
 
 
+def predicting(content):
+    content = stemming(content)
+    content = numpy.array(content, ndmin=1)
+    content = vectorizer.transform(content)
+    y_pred = model.predict(content)
+    print(y_pred)
+    return y_pred
 
 
 # UI design
-
-st.title("Fake news")
+title = '<h1 style="border-bottom: 2px solid white" >Fake News Detection</h1>'
+st.markdown(title, unsafe_allow_html=True)
 st.subheader("Check whether the news is fake or real 😶‍🌫️")
+
 textarea = st.text_area("")
 
-button= st.button("Predict")
+clicked = st.button("Predict")
+if clicked:
+    result = predicting(textarea)
+    real_news = '<p style="color:#33cc33; font-weight:700; font-size: 30px;">Real News</p>'
+    fake_news = '<p style="color:red; font-weight:700; font-size: 30px;">Fake News</p>'
+    with st.spinner('Wait for it...'):
+        time.sleep(1)
+    if result[0] == 0:
+        st.markdown(real_news, unsafe_allow_html=True)
+        st.snow()
+    else:
+        st.markdown(fake_news, unsafe_allow_html=True)
